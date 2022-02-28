24d231fb9a6e28adff4f2d2321d03df4f9b7c11f
ceciliawilson <90656220+ceciliawilson@users.noreply.github.com>
2022-02-28 10:42:04 -0800

    Add files via upload

---
+++ Text Adventure Build 2.py
@@ -0,0 +1,327 @@
+Python 3.10.0 (v3.10.0:b494f5935c, Oct  4 2021, 14:59:20) [Clang 12.0.5 (clang-1205.0.22.11)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======= RESTART: /Users/ceciliawilson/Documents/Text Adventure Build 2.py ======
+It is dark and you are in a rattling box, moving upward. As you approach the light you see a group of people crowding around you. You see the maze. Tall walls of rock.
+Now you must lead the group to safety but be careful of the grievers, flesh eating robots. AND USE THE NUMBER FOR YOUR ANSWER, DONT WRITE IT!
+Which way will you turn first? Right or Left?
+>right
+You turn right and run into a griever. Oh no! What will you do?
+1. Duck 2. Run away 3. Fight it
+>3
+You got lucky! Now run to the Glade
+You have escaped the griever and are now at the Glade
+Continue forward and look for the door
+Where will you go now?
+1.Right 2. Forward
+>2
+You are so close now but you need to find the key/code.
+To find the key you need to do some simple math
+What is 2+4
+>6
+Wow! You have made it to the door, the last thing standing between you and freedom. But you need a code to open it. You don't have the code, who do you ask for it?
+Be careful to not ask the snake
+1. Teresa 2. Newt 3. Minho
+>3
+YAY! ALWAYS TRUST MINHO! HE'S THE BEST
+You have escaped the maze!
+You are in the griever HQ
+You and the group are walking through a dark hallway and you reach a door. You open it and see a screen that's playing a video of a blonde woman. What do you do?
+1. Smash the screen 2. Keep watching
+>2
+Oh no! There is DEADLY disease and you are the key to everyone's survival. Be carful out there.
+To be continued...
+Do you want to play again?(y or n)
+>
+======= RESTART: /Users/ceciliawilson/Documents/Text Adventure Build 2.py ======
+It is dark and you are in a rattling box, moving upward. As you approach the light you see a group of people crowding around you. You see the maze. Tall walls of rock.
+Now you must lead the group to safety but be careful of the grievers, flesh eating robots. AND USE THE NUMBER FOR YOUR ANSWER, DONT WRITE IT!
+Which way will you turn first? Right or Left?
+>left
+You now have run into a huge griever. You only have one way of getting away. What will you do?
+1. Fight it 2. Run away
+>
+======= RESTART: /Users/ceciliawilson/Documents/Text Adventure Build 2.py ======
+It is dark and you are in a rattling box, moving upward. As you approach the light you see a group of people crowding around you. You see the maze. Tall walls of rock.
+You are in the Glade. Time to get adjusted. What job will you choose? AND USE THE NUMBER FOR YOUR ANSWER, DONT WRITE IT!
+A. Farmer B. Cook C. Builder
+>b
+Good choice. Let's get started!
+You are in the woods shortly after your arrival. One of the other Glade members attacks you. What do you do?
+1. Fight back 2. Run
+>2
+Smart idea! Get to the other Gladers and let them know what happened.
+You have made it to the maze. Now you must lead the group to safety but be careful of the grievers, flesh eating robots.
+Which way will you turn first? Right or Left?
+>left
+You now have run into a huge griever. You only have one way of getting away. What will you do?
+1. Fight it 2. Run away
+>2
+no bro, what?! you can't take a griever. You're done.
+GAME OVER!
+Do you want to play again?(y or n)
+>
+======= RESTART: /Users/ceciliawilson/Documents/Text Adventure Build 2.py ======
+It is dark and you are in a rattling box, moving upward. As you approach the light you see a group of people crowding around you. You see the maze. Tall walls of rock.
+You are in the Glade. Time to get adjusted. What job will you choose? AND USE THE NUMBER FOR YOUR ANSWER, DONT WRITE IT!
+A. Farmer B. Cook C. Builder
+>c
+Good choice. Let's get started!
+You are in the woods shortly after your arrival. One of the other Glade members attacks you. What do you do?
+1. Fight back 2. Run
+>2
+Smart idea! Get to the other Gladers and let them know what happened.
+You have made it to the maze. Now you must lead the group to safety but be careful of the grievers, flesh eating robots.
+Which way will you turn first? Right or Left?
+>r
+You turn right and run into a griever. Oh no! What will you do?
+1. Duck 2. Run away 3. Fight it
+>2
+You got lucky! Now run to the Glade
+You have escaped the griever and are now at the Glade
+Continue forward and look for the door
+Where will you go now?
+1.Right 2. Forward
+>2
+You are so close now but you need to find the key/code.
+To find the key you need to do some simple math
+What is 2+4
+>6
+Wow! You have made it to the door, the last thing standing between you and freedom. But you need a code to open it. You don't have the code, who do you ask for it?
+Be careful to not ask the snake
+1. Teresa 2. Newt 3. Minho
+>3
+YAY! ALWAYS TRUST MINHO! HE'S THE BEST
+You have escaped the maze!
+You are in the griever HQ
+You and the group are walking through a dark hallway and you reach a door. You open it and see a screen that's playing a video of a blonde woman. What do you do?
+1. Smash the screen 2. Keep watching
+>2
+Oh no! There is DEADLY disease and you are the key to everyone's survival. Be carful out there.
+To be continued...
+Do you want to play again?(y or n)
+>y
+It is dark and you are in a rattling box, moving upward. As you approach the light you see a group of people crowding around you. You see the maze. Tall walls of rock.
+You are in the Glade. Time to get adjusted. What job will you choose? AND USE THE NUMBER FOR YOUR ANSWER, DONT WRITE IT!
+A. Farmer B. Cook C. Builder
+>
+======= RESTART: /Users/ceciliawilson/Documents/Text Adventure Build 2.py ======
+It is dark and you are in a rattling box, moving upward. As you approach the light you see a group of people crowding around you. You see the maze. Tall walls of rock.
+You are in the Glade. Time to get adjusted. What job will you choose? AND USE THE NUMBER FOR YOUR ANSWER, DONT WRITE IT!
+1. Farmer 2. Cook 3. Builder
+>2
+Good choice. Let's get started!
+You are in the woods shortly after your arrival. One of the other Glade members attacks you. What do you do?
+1. Fight back 2. Run
+>1
+You can't take this guy. You are too weak and he is crazy. Try again!
+GAME OVER!
+Do you want to play again?(y or n)
+>y
+It is dark and you are in a rattling box, moving upward. As you approach the light you see a group of people crowding around you. You see the maze. Tall walls of rock.
+You are in the Glade. Time to get adjusted. What job will you choose? AND USE THE NUMBER FOR YOUR ANSWER, DONT WRITE IT!
+1. Farmer 2. Cook 3. Builder
+>1
+Good choice. Let's get started!
+You are in the woods shortly after your arrival. One of the other Glade members attacks you. What do you do?
+1. Fight back 2. Run
+>2
+Smart idea! Get to the other Gladers and let them know what happened.
+You have made it to the maze. Now you must lead the group to safety but be careful of the grievers, flesh eating robots.
+Which way will you turn first? Right or Left?
+>l
+You now have run into a huge griever. You only have one way of getting away. What will you do?
+1. Fight it 2. Run away
+>2
+no bro, what?! you can't take a griever. You're done.
+GAME OVER!
+Do you want to play again?(y or n)
+>
+======= RESTART: /Users/ceciliawilson/Documents/Text Adventure Build 2.py ======
+It is dark and you are in a rattling box, moving upward. As you approach the light you see a group of people crowding around you. You see the maze. Tall walls of rock.
+You are in the Glade. Time to get adjusted. What job will you choose? AND USE THE NUMBER FOR YOUR ANSWER, DONT WRITE IT!
+1. Farmer 2. Cook 3. Builder
+>3
+Good choice. Let's get started!
+You are in the woods shortly after your arrival. One of the other Glade members attacks you. What do you do?
+1. Fight back 2. Run
+>2
+Smart idea! Get to the other Gladers and let them know what happened.
+You have made it to the maze. Now you must lead the group to safety but be careful of the grievers, flesh eating robots.
+Which way will you turn first? Right or Left?
+>l
+You now have run into a huge griever. You only have one way of getting away. What will you do?
+1. Fight it 2. Run away
+>2
+Good Choice! Let's keep moving. To the Glade!
+You have escaped the griever and are now at the Glade
+Continue forward and look for the door
+Where will you go now?
+1.Right 2. Forward
+>1
+do you not listen to basic instructions
+GAME OVER!
+Do you want to play again?(y or n)
+>y
+It is dark and you are in a rattling box, moving upward. As you approach the light you see a group of people crowding around you. You see the maze. Tall walls of rock.
+You are in the Glade. Time to get adjusted. What job will you choose? AND USE THE NUMBER FOR YOUR ANSWER, DONT WRITE IT!
+1. Farmer 2. Cook 3. Builder
+>2
+Good choice. Let's get started!
+You are in the woods shortly after your arrival. One of the other Glade members attacks you. What do you do?
+1. Fight back 2. Run
+>1
+You can't take this guy. You are too weak and he is crazy. Try again!
+GAME OVER!
+Do you want to play again?(y or n)
+>y
+It is dark and you are in a rattling box, moving upward. As you approach the light you see a group of people crowding around you. You see the maze. Tall walls of rock.
+You are in the Glade. Time to get adjusted. What job will you choose? AND USE THE NUMBER FOR YOUR ANSWER, DONT WRITE IT!
+1. Farmer 2. Cook 3. Builder
+>2
+Good choice. Let's get started!
+You are in the woods shortly after your arrival. One of the other Glade members attacks you. What do you do?
+1. Fight back 2. Run
+>2
+Smart idea! Get to the other Gladers and let them know what happened.
+You have made it to the maze. Now you must lead the group to safety but be careful of the grievers, flesh eating robots.
+Which way will you turn first? Right or Left?
+>l
+You now have run into a huge griever. You only have one way of getting away. What will you do?
+1. Fight it 2. Run away
+>2
+Good Choice! Let's keep moving. To the Glade!
+You have escaped the griever and are now at the Glade
+Continue forward and look for the door
+Where will you go now?
+1.Right 2. Forward
+>2
+You are so close now but you need to find the key/code.
+To find the key you need to do some simple math
+What is 2+4
+>6
+Wow! You have made it to the door, the last thing standing between you and freedom. But you need a code to open it. You don't have the code, who do you ask for it?
+Be careful to not ask the snake
+1. Teresa 2. Newt 3. Minho
+>3
+YAY! ALWAYS TRUST MINHO! HE'S THE BEST
+You have escaped the maze!
+You are in the griever HQ
+You and the group are walking through a dark hallway and you reach a door. You open it and see a screen that's playing a video of a blonde woman. What do you do?
+1. Smash the screen 2. Keep watching
+>2
+Oh no! There is DEADLY disease and you are the key to everyone's survival. Be carful out there.
+To be continued...
+Do you want to play again?(y or n)
+>y
+It is dark and you are in a rattling box, moving upward. As you approach the light you see a group of people crowding around you. You see the maze. Tall walls of rock.
+You are in the Glade. Time to get adjusted. What job will you choose? AND USE THE NUMBER FOR YOUR ANSWER, DONT WRITE IT!
+1. Farmer 2. Cook 3. Builder
+>3
+Good choice. Let's get started!
+You are in the woods shortly after your arrival. One of the other Glade members attacks you. What do you do?
+1. Fight back 2. Run
+>2
+Smart idea! Get to the other Gladers and let them know what happened.
+You have made it to the maze. Now you must lead the group to safety but be careful of the grievers, flesh eating robots.
+Which way will you turn first? Right or Left?
+>right
+You turn right and run into a griever. Oh no! What will you do?
+1. Duck 2. Run away 3. Fight it
+>3
+You got lucky! Now run to the Glade
+You have escaped the griever and are now at the Glade
+Continue forward and look for the door
+Where will you go now?
+1.Right 2. Forward
+>2
+You are so close now but you need to find the key/code.
+To find the key you need to do some simple math
+What is 2+4
+>6
+Wow! You have made it to the door, the last thing standing between you and freedom. But you need a code to open it. You don't have the code, who do you ask for it?
+Be careful to not ask the snake
+1. Teresa 2. Newt 3. Minho
+>2
+Wrong! He's awesome so I understand but no
+GAME OVER!
+Do you want to play again?(y or n)
+>y
+It is dark and you are in a rattling box, moving upward. As you approach the light you see a group of people crowding around you. You see the maze. Tall walls of rock.
+You are in the Glade. Time to get adjusted. What job will you choose? AND USE THE NUMBER FOR YOUR ANSWER, DONT WRITE IT!
+1. Farmer 2. Cook 3. Builder
+>3
+Good choice. Let's get started!
+You are in the woods shortly after your arrival. One of the other Glade members attacks you. What do you do?
+1. Fight back 2. Run
+>2
+Smart idea! Get to the other Gladers and let them know what happened.
+You have made it to the maze. Now you must lead the group to safety but be careful of the grievers, flesh eating robots.
+Which way will you turn first? Right or Left?
+>right
+You turn right and run into a griever. Oh no! What will you do?
+1. Duck 2. Run away 3. Fight it
+>3
+You got lucky! Now run to the Glade
+You have escaped the griever and are now at the Glade
+Continue forward and look for the door
+Where will you go now?
+1.Right 2. Forward
+>2
+You are so close now but you need to find the key/code.
+To find the key you need to do some simple math
+What is 2+4
+>6
+Wow! You have made it to the door, the last thing standing between you and freedom. But you need a code to open it. You don't have the code, who do you ask for it?
+Be careful to not ask the snake
+1. Teresa 2. Newt 3. Minho
+>3
+YAY! ALWAYS TRUST MINHO! HE'S THE BEST
+You have escaped the maze!
+You are in the griever HQ
+You and the group are walking through a dark hallway and you reach a door. You open it and see a screen that's playing a video of a blonde woman. What do you do?
+1. Smash the screen 2. Keep watching
+>1
+Oh no! The room is on fire, you killed everyone. Good job!
+GAME OVER!
+Do you want to play again?(y or n)
+>y
+It is dark and you are in a rattling box, moving upward. As you approach the light you see a group of people crowding around you. You see the maze. Tall walls of rock.
+You are in the Glade. Time to get adjusted. What job will you choose? AND USE THE NUMBER FOR YOUR ANSWER, DONT WRITE IT!
+1. Farmer 2. Cook 3. Builder
+>3
+Good choice. Let's get started!
+You are in the woods shortly after your arrival. One of the other Glade members attacks you. What do you do?
+1. Fight back 2. Run
+>2
+Smart idea! Get to the other Gladers and let them know what happened.
+You have made it to the maze. Now you must lead the group to safety but be careful of the grievers, flesh eating robots.
+Which way will you turn first? Right or Left?
+>right
+You turn right and run into a griever. Oh no! What will you do?
+1. Duck 2. Run away 3. Fight it
+>3
+You got lucky! Now run to the Glade
+You have escaped the griever and are now at the Glade
+Continue forward and look for the door
+Where will you go now?
+1.Right 2. Forward
+>2
+You are so close now but you need to find the key/code.
+To find the key you need to do some simple math
+What is 2+4
+>6
+Wow! You have made it to the door, the last thing standing between you and freedom. But you need a code to open it. You don't have the code, who do you ask for it?
+Be careful to not ask the snake
+1. Teresa 2. Newt 3. Minho
+>3
+YAY! ALWAYS TRUST MINHO! HE'S THE BEST
+You have escaped the maze!
+You are in the griever HQ
+You and the group are walking through a dark hallway and you reach a door. You open it and see a screen that's playing a video of a blonde woman. What do you do?
+1. Smash the screen 2. Keep watching
+>2
+Oh no! There is DEADLY disease and you are the key to everyone's survival. Be carful out there.
+To be continued...
+Do you want to play again?(y or n)
+>
